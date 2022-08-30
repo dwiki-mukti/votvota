b789d9c8db492c6a5c8fe5196ecebf317a15c0df
2-Q <243dwiki.com@gmail.com>
2022-08-30 18:36:04 +0700

    footer

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -7,8 +7,7 @@
     <title>AdminLTE 3 | Dashboard</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
     <!-- icon -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
@@ -21,27 +20,23 @@
 
     @yield('style')
 </head>
-{{--  sidebar-mini layout-fixed --}}
+{{-- sidebar-mini layout-fixed --}}
+
 <body class="hold-transition layout-top-nav">
     <div class="wrapper">
-
-        <!-- Preloader -->
-        <!-- <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset(env('APP_ICON')) }}" alt="AdminLTELogo" height="60" width="60">
-        </div> -->
 
         @include('layouts.components.navbar')
         {{-- @include('layouts.components.sidebar') --}}
 
-        <div class="content-wrapper">
+        <div class="content-wrapper pb-4">
             @yield('content')
         </div>
 
-        <footer class="main-footer mt-4">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-            All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+        <footer class="main-footer">
+            <div class="text-right">
+                &copy;<script>
+                    document.write(new Date().getFullYear());
+                </script> by Wongedanyongkru
             </div>
         </footer>
     </div>
